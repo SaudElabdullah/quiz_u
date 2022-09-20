09f133fce55115e3b9a8e8829d560c394d7d10cd
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-20 23:44:00 +0300

    chore: modified login controller

--- lib/app/login/controllers/login_controller.dart
+++ lib/app/login/controllers/login_controller.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/cupertino.dart';
-import 'package:phone_number/phone_number.dart';
 
 class LoginController {
-  final PageController pageController = PageController(initialPage: 0, keepPage: true);
+  final PageController pageController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
   final phoneController = TextEditingController();
   final smsCodeController = TextEditingController();
-
-  Future<bool> validatePhoneNumber(String phoneNumber) async {
-    return await PhoneNumberUtil().validate('+966${phoneNumber.substring(1)}');
-  }
+  final nameController = TextEditingController();
 
   String? smsValidator(String? sms) {
-    if ((sms?.length ?? 0) < 6) return "Incorrect SMS Code";
+    if (((sms?.length ?? 0) < 6) || sms != '0000') return "Incorrect SMS Code";
     return null;
+  }
+
+  void navigateToNextPage() {
+    pageController.nextPage(
+      duration: const Duration(
+        milliseconds: 200,
+      ),
+      curve: Curves.ease,
+    );
   }
 }
