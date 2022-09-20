e7124df82d55cafe6ae334bfeda4b3e5b509e460
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-20 23:43:43 +0300

    chore: created login function

--- lib/app/login/services/login_services.dart
+++ lib/app/login/services/login_services.dart
@@ -1,3 +1,21 @@
-class LoginServices{
+import 'dart:convert' as convert;
 
+import 'package:http/http.dart' as http;
+
+class LoginServices {
+  static Future<dynamic> login(String otp, String phoneNumber) async {
+    var map = {};
+    map['OTP'] = otp;
+    map['mobile'] = phoneNumber;
+    var request = await http.post(
+      Uri.parse(
+        'https://quizu.okoul.com/Login',
+      ),
+      body: {
+        'OTP': otp,
+        'mobile': phoneNumber,
+      },
+    );
+    return await convert.json.decode(request.body);
+  }
 }
