d277c0856d4ea5b2660deb1916496347df98588b
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-25 22:17:27 +0300

    chore: rearrange the parameters

--- lib/app/profile/services/profile_services.dart
+++ lib/app/profile/services/profile_services.dart
@@ -4,10 +4,10 @@ import 'package:http/http.dart' as http;
 import 'package:quiz_u/app/login/models/user.dart';
 
 class ProfileServices {
-  Future<User> getUserInformation(String token) async {
+  static Future<User> getUserInformation(String token) async {
     var headers = {
       'Authorization':
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNjYzMzU4NDY1fQ.LlVAcArd2Bn3gtdanoHlfMOsHn0gRMqvVHozUk4bjWM'
+          'Bearer $token'
     };
     var request = http.Request(
       'GET',
