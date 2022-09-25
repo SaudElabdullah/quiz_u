a2c625ece04d7d1e1d276e6d33d0232f0b994024
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-25 22:16:54 +0300

    chore: added functions

--- lib/app/login/services/login_services.dart
+++ lib/app/login/services/login_services.dart
@@ -17,16 +17,16 @@ class LoginServices {
     return await convert.json.decode(request.body);
   }
 
-  static Future<dynamic> registerName(String name) async {
+  static Future<dynamic> registerName(String name, String token,) async {
     var request = await http.post(
       Uri.parse(
         'https://quizu.okoul.com/Name',
       ),
       headers: {
         'Authorization':
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjM3OTQyNzd9.ft-Mo0BF2gdiaiD7jOPVqAlVx5z_D7Chy_5cl1u77UE',
+            'Bearer $token',
       },
-      body: {'name': 'Saud'},
+      body: {'name': name},
     );
     return await convert.json.decode(request.body);
   }
@@ -34,7 +34,7 @@ class LoginServices {
   static Future<bool> tokenVerification(String name, String token) async {
     var headers = {
       'Authorization':
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjM3OTQ3NDR9.TuIgP-ORS-LBH9OFc3QQ1HFq8cz2OVRSqw9HbXlXVSY',
+          'Bearer $token',
       'Content-Type': 'application/json'
     };
     var request = http.Request(
@@ -45,7 +45,7 @@ class LoginServices {
     );
     request.body = json.encode(
       {
-        "name": "Ahmed",
+        "name": name,
       },
     );
     request.headers.addAll(headers);
@@ -53,5 +53,23 @@ class LoginServices {
     http.StreamedResponse response = await request.send();
 
     return (jsonDecode(await response.stream.bytesToString()) as Map<String, dynamic>)['succes'] as bool;
+  }
+
+  Future<void> postUserScore(String token, String score) async {
+    var headers = {
+      'Authorization':
+          'Bearer $token',
+      'Content-Type': 'application/json'
+    };
+    var request = http.Request(
+      'POST',
+      Uri.parse(
+        'https://quizu.okoul.com/Score',
+      ),
+    );
+    request.body = json.encode({"score": "11"});
+    request.headers.addAll(headers);
+
+    await request.send();
   }
 }
